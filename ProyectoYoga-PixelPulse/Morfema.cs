2c390d9c4c93d32423a8e878442e2990aa59bd90
CATEVA26 <carlos.landazuri@epn.edu.ec>
2023-11-25 12:49:56 -0500

    creación clase Morfema y archivo de Configuración

---
+++ ProyectoYoga-PixelPulse/Morfema.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoYoga_PixelPulse
+{
+    internal class Morfema
+    {
+        public static Dictionary<string, string> diccionarioSansEs = new Dictionary<string, string>() {
+    {"Asana",Res.postura} ,
+    {"Adho",Res.abajo},
+    {"Ardha",Res.mitad},
+    {"Urdhva",Res.arriba},
+    {"Mukha",Res.rostro},
+    {"Svana",Res.perro},
+    {"Virabhadra",Res.guerrero},
+    {"Vriksha",Res.arbol} ,
+    {"Bala",Res.ninio},
+    {"Bhujanga",Res.cobra},
+    {"Paschi",Res.oeste},
+    {"Setu",Res.puente},
+    {"Shava",Res.cadaver},
+    {"Trikona",Res.triangulo},
+    {"Matsya",Res.pez},
+    {"Mala",Res.guirnalda},
+    {"Eka",Res.uno},
+    {"Pada",Res.pie},
+    {"Raja",Res.real},
+    {"Kapota:",Res.paloma},
+    {"Hasta",Res.mano},
+    {"Parivrtta",Res.invertido},
+    {"Janu", Res.rodilla},
+    {"Sirsa", Res.cabeza}
+    };
+       
+        private static List<string> TraducirMorfema(string? sans)
+        {
+            string[] palabras = sans.Split(" ");
+            List<string> morfemas = new List<string>();
+
+            foreach (string palabra in palabras)
+            {
+                if (diccionarioSansEs.ContainsKey(palabra))
+                {
+                    morfemas.Add(palabra+":"+ diccionarioSansEs[palabra]);
+                }
+                else
+                {
+                    string asana = palabra.Replace("asana", "a Asana");
+                    TraducirMorfema(asana);
+                }
+
+
+            }
+            return morfemas;
+
+        }
+
+
+
+        private static void ConvertirEn()
+        {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+        }
+
+
+    }
+}
