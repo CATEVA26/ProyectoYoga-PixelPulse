5881a4f2f1498df59ce167e6cf38744eebaceeb4
CATEVA26 <carlos.landazuri@epn.edu.ec>
2023-12-02 13:34:07 -0500

    Traduccion a Ingles

--- ProyectoYoga-PixelPulse/DiccionarioYoga.cs
+++ ProyectoYoga-PixelPulse/DiccionarioYoga.cs
@@ -27,12 +27,17 @@ namespace ProyectoYoga_PixelPulse
 
         }
 
-        private void BusquedaButton_Click(object sender, EventArgs e)
-        {
+        private async void  BusquedaButton_Click(object sender, EventArgs e)
+        {   
             string textoAMorfema = BusquedaTextBox.Text;
+            var Traductor = new Traductor();
             Postura postura = new Postura(textoAMorfema);
             MorfemaTextBox.Text = string.Join("\r\n", postura.GetMorfemas());
             ESTextBox.Text = postura.GetDescripción();
+            ENTextBox.Text = await Traductor.Traducir(postura.GetDescripción());
+            if (ENTextBox.Equals(string.Empty)) {
+                MessageBox.Show("Lo sentimos no se ha realizado la traducción");
+            }
         }
     }
 }
---
+++ ProyectoYoga-PixelPulse/Traductor.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoYoga_PixelPulse
+{
+    internal class Traductor
+    {
+        private readonly HttpClient _httpClient;
+        public class TranslationResult
+        {
+            public string TranslationText { get; set; }
+        }
+        public Traductor() {
+            _httpClient = new();
+        
+        }
+        public async Task<string> Traducir(string inputText)
+        {
+            string apiUrl = "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-es-en";
+            string apiKey = "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";
+
+            try
+            {
+                // Configurar encabezados de la solicitud
+                _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+                // Crear objeto con la estructura esperada por la API
+                var requestData = new { inputs = inputText };
+
+                // Convertir objeto a cadena JSON
+                string jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(requestData);
+
+                // Crear contenido para la solicitud POST
+                var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+                // Realizar solicitud POST a la API
+                HttpResponseMessage response = await _httpClient.PostAsync(apiUrl, content);
+
+                // Verificar si la solicitud fue exitosa (código de estado 200-299)
+                if (response.IsSuccessStatusCode)
+                {
+                    // Leer el contenido de la respuesta como una cadena
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    
+                    // Deserializar el JSON al objeto TranslationResult
+
+                    TranslationResult[] resultArray = Newtonsoft.Json.JsonConvert.DeserializeObject<TranslationResult[]>(responseBody);
+
+                    MessageBox.Show(responseBody);
+                    return resultArray?[0]?.TranslationText;
+                }
+                else
+                {
+                    return $"Error en la solicitud: {response.StatusCode} - {response.ReasonPhrase}";
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Error en la solicitud: {ex.Message}";
+            }
+        }
+    }
+}
